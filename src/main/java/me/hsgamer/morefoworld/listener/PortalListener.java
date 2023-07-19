11a2dc4f2d88901a6de667128c119e0bf3755b5a
HSGamer <huynhqtienvtag@gmail.com>
2023-07-19 22:58:34 +0700

    Nether ready event

--- src/main/java/me/hsgamer/morefoworld/listener/PortalListener.java
+++ src/main/java/me/hsgamer/morefoworld/listener/PortalListener.java
@@ -1,14 +1,10 @@
 package me.hsgamer.morefoworld.listener;
 
+import io.papermc.paper.event.entity.EntityPortalReadyEvent;
 import me.hsgamer.morefoworld.MoreFoWorld;
-import org.bukkit.Location;
-import org.bukkit.World;
+import org.bukkit.PortalType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityPortalEvent;
-import org.bukkit.event.player.PlayerPortalEvent;
-
-import java.util.Optional;
 
 public class PortalListener implements Listener {
     private final MoreFoWorld plugin;
@@ -18,45 +14,12 @@ public class PortalListener implements Listener {
     }
 
     @EventHandler
-    public void onPlayerPortal(PlayerPortalEvent event) {
-        Location from = event.getFrom();
-        Location to = event.getTo();
-        plugin.debug("Portal Cause: " + event.getCause());
-        plugin.debug("From: " + from);
-        plugin.debug("To: " + to);
-        Optional<World> worldOptional = switch (event.getCause()) {
-            case NETHER_PORTAL -> plugin.getPortalConfig().getWorldFromNetherPortal(from.getWorld());
-            case END_PORTAL -> plugin.getPortalConfig().getWorldFromEndPortal(from.getWorld());
-            default -> Optional.empty();
-        };
-        worldOptional.ifPresent(world -> {
-            Location clone = to.clone();
-            clone.setWorld(world);
-            event.setTo(clone);
-            plugin.debug("Set Portal to " + clone);
-        });
-    }
-
-    @EventHandler
-    public void onEntityPortal(EntityPortalEvent event) {
-        Location from = event.getFrom();
-        Location to = event.getTo();
-        plugin.debug("Entity Portal: " + event.getPortalType());
-        plugin.debug("From: " + from);
-        plugin.debug("To: " + to);
-        if (to == null) {
-            return;
-        }
-        Optional<World> worldOptional = switch (event.getPortalType()) {
-            case NETHER -> plugin.getPortalConfig().getWorldFromNetherPortal(from.getWorld());
-            case ENDER -> plugin.getPortalConfig().getWorldFromEndPortal(from.getWorld());
-            default -> Optional.empty();
-        };
-        worldOptional.ifPresent(world -> {
-            Location clone = to.clone();
-            clone.setWorld(world);
-            event.setTo(clone);
-            plugin.debug("Set Portal to " + clone);
+    public void onPortalReady(EntityPortalReadyEvent event) {
+        if (event.getPortalType() != PortalType.NETHER) return;
+        plugin.debug("Portal Ready: " + event.getEntity().getWorld());
+        plugin.getPortalConfig().getWorldFromNetherPortal(event.getEntity().getWorld()).ifPresent(world -> {
+            event.setTargetWorld(world);
+            plugin.debug("Set Portal to " + world);
         });
     }
 }
