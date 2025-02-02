accbf2a8a1f404787c99f7b5e6aa888d11025335
hsgamer <huynhqtienvtag@gmail.com>
2025-02-02 17:42:54 +0700

    schedule respawn event

--- src/main/java/me/hsgamer/morefoworld/listener/RespawnListener.java
+++ src/main/java/me/hsgamer/morefoworld/listener/RespawnListener.java
@@ -4,9 +4,12 @@ import io.github.projectunified.minelib.plugin.base.BasePlugin;
 import io.github.projectunified.minelib.plugin.listener.ListenerComponent;
 import me.hsgamer.morefoworld.DebugComponent;
 import me.hsgamer.morefoworld.config.RespawnConfig;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 import java.util.Optional;
 
@@ -29,18 +32,36 @@ public class RespawnListener implements ListenerComponent {
     }
 
     @EventHandler
-    public void onRespawn(PlayerRespawnEvent event) {
-        debug.debug("Respawn: " + event.getPlayer().getName() + " in " + event.getRespawnLocation());
-        debug.debug("Reason: " + event.getRespawnReason());
+    public void onDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        Location location = player.getLocation();
+        debug.debug("Death: " + player.getName() + " at " + location);
 
-        Optional.ofNullable(event.getPlayer().getLastDeathLocation())
-                .map(Location::getWorld)
-                .flatMap(plugin.get(RespawnConfig.class)::getRespawnWorld)
-                .ifPresent(world -> {
-                    Location respawnLocation = event.getRespawnLocation();
-                    respawnLocation.setWorld(world);
-                    event.setRespawnLocation(respawnLocation);
-                    debug.debug("Set Respawn to " + world);
-                });
+        World world = location.getWorld();
+        Optional<World> optionalRespawnWorld = plugin.get(RespawnConfig.class).getRespawnWorld(world);
+        if (optionalRespawnWorld.isEmpty()) return;
+        World respawnWorld = optionalRespawnWorld.get();
+        Location respawnLocation = respawnWorld.getSpawnLocation();
+        debug.debug("Set Respawn to " + respawnWorld);
+
+        RespawnRunnable runnable = new RespawnRunnable(player, respawnLocation);
+        player.getScheduler().runAtFixedRate(plugin, t -> runnable.run(), null, 0, 1);
+        debug.debug("Scheduled Respawn");
+    }
+
+    private class RespawnRunnable implements Runnable {
+        private final Player player;
+        private final Location location;
+
+        private RespawnRunnable(Player player, Location location) {
+            this.player = player;
+            this.location = location;
+        }
+
+        @Override
+        public void run() {
+            if (player.isDead()) return;
+            Bukkit.getRegionScheduler().execute(plugin, location, () -> player.teleportAsync(location));
+        }
     }
 }
