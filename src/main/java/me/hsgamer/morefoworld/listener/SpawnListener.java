d1c73e5dab7ca9b7feda2f48d958e1fc354239e1
hsgamer <huynhqtienvtag@gmail.com>
2025-02-02 17:28:17 +0700

    schedule task to teleport to spawn location on join

--- src/main/java/me/hsgamer/morefoworld/listener/SpawnListener.java
+++ src/main/java/me/hsgamer/morefoworld/listener/SpawnListener.java
@@ -3,8 +3,11 @@ package me.hsgamer.morefoworld.listener;
 import io.github.projectunified.minelib.plugin.base.BasePlugin;
 import io.github.projectunified.minelib.plugin.listener.ListenerComponent;
 import me.hsgamer.morefoworld.config.SpawnConfig;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.spigotmc.event.player.PlayerSpawnLocationEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class SpawnListener implements ListenerComponent {
     private final BasePlugin plugin;
@@ -19,12 +22,14 @@ public class SpawnListener implements ListenerComponent {
     }
 
     @EventHandler
-    public void onSpawn(PlayerSpawnLocationEvent event) {
+    public void onJoin(PlayerJoinEvent event) {
         SpawnConfig spawnConfig = plugin.get(SpawnConfig.class);
 
         if (!spawnConfig.isEnabled()) return;
         if (spawnConfig.isFirstJoin() && !event.getPlayer().hasPlayedBefore()) return;
 
-        event.setSpawnLocation(spawnConfig.getPosition().toLocation());
+        Player player = event.getPlayer();
+        Location location = spawnConfig.getPosition().toLocation();
+        Bukkit.getRegionScheduler().execute(plugin, location, () -> player.teleportAsync(location));
     }
 }
